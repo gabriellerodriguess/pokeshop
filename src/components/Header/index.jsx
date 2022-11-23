ec70775e89823ae666878723252af570ab08ce48
Gabrielle Rodrigues <gabriellerut@gmail.com>
2022-11-23 20:09:44 -0300

    feat/004: create menu hamburguer in mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,48 @@
+import { useEffect, useState } from 'react'
 import {FaShoppingCart} from 'react-icons/fa'
+import {GiHamburgerMenu} from 'react-icons/gi'
 import './styles.css'
 
-export default function Header() {
+export default function Header() { 
+    const [isMobile, setIsMobile] = useState(false)
+    const [menuVisible, setMenuVisible] = useState(false)
+
+    useEffect(() => {
+        menuMobile()
+    }, [])
+    
+    function menuMobile() {
+        if(window.innerWidth <= 768) {
+            setIsMobile(!isMobile)
+        }
+    }
+    
+    function handleClick() {
+        setMenuVisible(!menuVisible)
+    }
+
     return (
         <header>
+            {isMobile && 
+                <div className='menu-mobile' onClick={() => handleClick()}> 
+                    <GiHamburgerMenu />
+                    <nav className={`nav-mobile ${menuVisible ? 'enabled' : 'disabled'}`}>
+                        <a href="#">Pokemons</a>
+                        <a href="#">Items</a>
+                        <a href="#">Shop</a>
+                    </nav>
+                </div>
+            }
             <div className='title-menu'>
                 <h1>Pokeshop</h1>
             </div>
-            <nav className='nav-menu'>
-                <a href="#">Pokemons</a>
-                <a href="#">Items</a>
-                <a href="#">Shop</a>
-            </nav>
+            {!isMobile &&
+                <nav className='menu-desk'>
+                    <a href="#">Pokemons</a>
+                    <a href="#">Items</a>
+                    <a href="#">Shop</a>
+                </nav>
+            }
             <div className='cart-menu'>
                 <span className='icon-cart'> <FaShoppingCart /> </span>
                 <span className="qtd-items"> 0 </span>
